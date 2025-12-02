57ddd3a00d45a291cf5aeb47b57a1a5058cd39ed
atahir <atahir@autopayplus.com>
2025-12-02 14:33:36 +0500

    changed port in backend

--- backend/server.js
+++ backend/server.js
@@ -325,6 +325,10 @@ app.get('/', (req, res) => {
 });
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on ${PORT}`);
+// app.listen(PORT, () => {
+//   console.log(`Server is running on ${PORT}`);
+// });
+
+app.listen(3000, '0.0.0.0', () => {
+  console.log('Server is running on 3000');
 });
